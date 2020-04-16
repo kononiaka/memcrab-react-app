a664e9479d9964a2da385c0aca9ce71ca770ec93
Kononiaka Vladimir <kononiaka.vladimir@gmail.com>
2020-04-16 21:13:22 +0300

    refactored

--- src/scripts/fun.js
+++ src/scripts/fun.js
@@ -18,40 +18,57 @@
   Дать возможность удалить строку с таблицы, при этом должны поменяться средние значения по каждому столбику
   Дать возможность добавить строку, фактически M + 1. При этом строка заполняется по всем правилам таблицы.
 */
-
-const table4 = {
-  table: [
-    [{ id: 1, amount: 543 }, { id: 3, amount: 567 }, { id: 2, amount: 342 }],
-    [{ id: 1, amount: 543 }, { id: 3, amount: 567 }, { id: 2, amount: 342 }],
-    [{ id: 1, amount: 543 }, { id: 3, amount: 567 }, { id: 2, amount: 342 }],
-    [{ id: 1, amount: 543 }, { id: 3, amount: 567 }, { id: 2, amount: 342 }],
-  ],
-  xSum: [8847, 4432, 5886],
-  ySum: [8847, 4432, 5886, 5463],
+const state = {
+  tableData: {
+    table: [
+      [
+        { id: 1, amount: 543 },
+        { id: 3, amount: 567 },
+        { id: 2, amount: 342 },
+      ],
+      [
+        { id: 1, amount: 543 },
+        { id: 3, amount: 567 },
+        { id: 2, amount: 342 },
+      ],
+      [
+        { id: 1, amount: 543 },
+        { id: 3, amount: 567 },
+        { id: 2, amount: 342 },
+      ],
+      [
+        { id: 1, amount: 543 },
+        { id: 3, amount: 567 },
+        { id: 2, amount: 342 },
+      ],
+    ],
+    rowTotals: [8847, 4432, 5886],
+    colTotals: [8847, 4432, 5886, 5463],
+  },
 };
 
 const randomInteger = (min, max) => {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
-}
+};
 
 const genID = (length = 10) => {
-  const chars = '123456789qwertyuiopasdfghjklzxcvbnm';
+  const chars = "123456789qwertyuiopasdfghjklzxcvbnm";
   const max = chars.length - 1;
 
-  let id = '';
+  let id = "";
   for (let i = 0; i < length; i++) {
     const index = randomInteger(0, max);
     id += chars[index];
   }
 
   return id;
-}
+};
 
 makeCell = () => ({
   id: genID(8),
   amount: randomInteger(100, 999),
-})
+});
 
 const makeTable = (colCount, rowCount) => {
   const table = [];
@@ -66,12 +83,33 @@ const makeTable = (colCount, rowCount) => {
   }
 
   return table;
-}
+};
+
+extractAmount = item => item.anount;
+const sum = nums => nums.reduce((total, n) => total + n, 0);
 
 const table = makeTable(5, 5);
-const xSum = table.map(row => row.map(cell => cell.amount).reduce((sum, num) => sum + num, 0));
-const ySum;
+const rowTotals = table.map(row =>
+  row.reduce((total, { amount }) => total + amount, 0)
+);
+const colTotals = table
+  .map((row, index) =>
+    table.reduce((total, row) => total + row[index].amount, 0)
+  )
+  .map(num => num / table[0].length)
+  .map(Math.round);
 
+// let colTotals = [];
+// const limit = table[0].length;
+
+// for (let i = 0; i < limit; i++) {
+//   let sum = 0;
+//   for (let row of table) {
+//     sum += row[i].amount;
+//   }
+//   colTotals.push(sum);
+// }
 
 console.log(table);
-console.log(xSum);
+console.log(rowTotals);
+console.log(colTotals);
